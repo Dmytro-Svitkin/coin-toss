55eebb76e41908bed2a434d36cb23fbd00ed6a70
Dmytro Svitkin <77505900+Dmytro-Svitkin@users.noreply.github.com>
2026-03-27 22:19:30 +0100

    Toss() finalise

--- js.js
+++ js.js
@@ -1,4 +1,14 @@
 let heads, tails, edge, bottom, clickArea;
+let state = "heads"
+
+function range(x, y = undefined, z = 1) {
+    if (y == undefined) {y = 0} else {let recol = x; x = y; y = recol;}
+    let recol = [];
+    for (let i = y; i < x; i += z) {
+        recol.push(i);
+    }
+    return recol;
+}
 
 function Start() {
     heads = document.getElementById("heads");
@@ -8,18 +18,13 @@ function Start() {
     clickArea = document.getElementById("click-area");
 };
 
-function Toss() {
-    let random = Math.floor(Math.random() * (12 - 5 + 1)) + 5;
-    let time = random/1.5;
-    console.log(random);
-    /*clickArea.style.display = "none";*/
+function FlipHeads() {      
     heads.style.opacity = "1"; tails.style.opacity = "0";
-    bottom.style.transformOrigin = "center 56.25%";
+    heads.style.transformOrigin = "center 43.75%"; tails.style.transformOrigin="center 56.25%"; bottom.style.transformOrigin = "center 56.25%";
     heads.style.transform = "scale(1, 0)";
     tails.style.transform = "scale(1, 0)";
     edge.style.transform = "scale(1, 1)";
     bottom.style.transform = "scale(1, 0)";
-
     setTimeout(() => {
         heads.style.opacity = "0"; tails.style.opacity = "1";
         bottom.style.transformOrigin = "center 43.75%";
@@ -28,6 +33,39 @@ function Toss() {
         edge.style.transform = "scale(1, 0)";
         bottom.style.transform = "scale(1, 1)";
     }, 310);
-    /*setTimeout(() => {clickArea.style.display = "block";}, random*1000-500);*/
+    state = "tails"
+}
+
+function FlipTails() {
+    heads.style.opacity = "0"; tails.style.opacity = "1";
+    heads.style.transformOrigin = "center 56.25%"; tails.style.transformOrigin="center 43.75%"; bottom.style.transformOrigin = "center 56.25%";
+    heads.style.transform = "scale(1, 0)";
+    tails.style.transform = "scale(1, 0)";
+    edge.style.transform = "scale(1, 1)";
+    bottom.style.transform = "scale(1, 0)";
+    setTimeout(() => {
+        heads.style.opacity = "1"; tails.style.opacity = "0";
+        bottom.style.transformOrigin = "center 43.75%";
+        heads.style.transform = "scale(1, 1)";
+        tails.style.transform = "scale(1, 1)";
+        edge.style.transform = "scale(1, 0)";
+        bottom.style.transform = "scale(1, 1)";
+    }, 310);
+    state = "heads"
+}
+
+function Flip(){
+    if (state == "heads"){FlipHeads()}
+    else if (state == "tails"){FlipTails()}
+}
+
+function Toss() {
+    let random = Math.floor(Math.random() * (12 - 5 + 1)) + 5;
+    console.log(random);
+    clickArea.style.display = "none";
+    for (let i of range(random)){
+        setTimeout(() => {Flip();}, 910*i)
+    }
+    setTimeout(() => {clickArea.style.display = "block";}, random*1000);
 };
 
