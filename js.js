afde8ea38852e1ef334a5cf1057a4d87e733e930
Dmytro Svitkin <77505900+Dmytro-Svitkin@users.noreply.github.com>
2026-03-27 18:27:09 +0100

    tansition

--- js.js
+++ js.js
@@ -5,6 +5,7 @@ function Start() {
     tails = document.getElementById("tails");
     edge = document.getElementById("edge");
     bottom = document.getElementById("bottom");
+    bottomT = document.getElementById("bottom-t");
     clickArea = document.getElementById("click-area");
 };
 
@@ -12,7 +13,17 @@ function Toss() {
     let random = Math.floor(Math.random() * (12 - 5 + 1)) + 5;
     let time = random/1.5;
     console.log(random);
-    clickArea.style.display = "none";
-    setTimeout(() => {clickArea.style.display = "block";}, 500);
+    /*clickArea.style.display = "none";*/
+    heads.style.transform = "scaleY(0)";
+    edge.style.transform = "scaleY(1)";
+    bottom.style.transform = "scaleY(0)";
+    bottomT.style.transform = "translateY(12.5%)";
+    setTimeout(() => {
+        heads.style.transform = "scaleY(1)";
+        edge.style.transform = "scaleY(0)";
+        bottom.style.transform = "scaleY(1)";
+        bottomT.style.transform = "translateY(0)";
+    }, 750);
+    /*setTimeout(() => {clickArea.style.display = "block";}, random*1000-500);*/
 };
 
